2622db0400e41810498f177240f439ad92c640c7
Nikita Kosenko <foxidalim@gmail.com>
2024-11-01 11:29:42 +0300

    Commit latest changes.

--- laba2/main.cpp
+++ laba2/main.cpp
@@ -3,9 +3,10 @@
 #include <iomanip>
 #include <iostream>
 #include <openssl/md5.h>
-#include <unistd.h>
 #include <optional>
 #include <sstream>
+#include <string.h>
+#include <unistd.h>
 #include <vector>
 
 #define ERROR_CREATE_THREAD -11
@@ -67,27 +68,10 @@ std::optional<std::string> iter_bytes(std::string pswd, std::string hash,
 
   if (hash == md5(pswd)) {
     found.store(true);
-    return pswd;
+    return std::optional<std::string>{pswd};
   }
 
   return std::nullopt;
-}
-
-std::optional<std::string> find_password(std::string args) {
-  std::stringstream ss(args);
-  std::string word;
-  std::vector<std::string> hash_start_stop;
-  while (getline(ss, word, ' ')) {
-    hash_start_stop.push_back(word);
-  }
-
-  auto res =
-      iter_bytes(hash_start_stop[1], hash_start_stop[0], hash_start_stop[2]);
-  if (res != std::nullopt) {
-    std::cout << "Found password, the password is: " << *res << std::endl;
-  }
-
-  return res;
 }
 
 std::optional<std::string> check_len(std::string line, std::size_t n) {
@@ -139,55 +123,173 @@ int main() {
   std::cout << "1 -- quit" << std::endl;
   std::cout << "2 -- input password" << std::endl;
   std::cout << "3 -- run single thread search of the password" << std::endl;
+  std::cout << "4 -- run parallel search via double fork()" << std::endl;
 
   char option;
   std::cout << ">> ";
   std::cin >> option;
-  while (option != '1') {
-    switch (option) {
-    case '2': {
-      std::cout
-          << "Input 6 bytes of password. Symbols a-z and 0-9 are permitted."
-          << std::endl;
-      std::cout << ">> ";
-      std::string pswd;
-      std::cin >> pswd;
-      auto res = check_line(pswd, 'p');
-      if (!res) {
-        std::cout << "Invalid password!" << std::endl;
-        break;
-      }
-
-      std::cout << "Hash for the password is: " << md5(pswd) << std::endl;
-      break;
-    }
-
-    case '3': {
-      std::cout
-          << "Input 32 symbols of hash-sum. Symbols a-f and 0-9 are permitted."
-          << std::endl;
-      std::cout << ">> ";
-      std::string hash;
-      std::cin >> hash;
-
-      auto res = check_line(hash, 'h');
-      if (!res) {
-        std::cout << "Invalid hash!" << std::endl;
-        break;
-      }
-
-      auto args = (*res + " " + "000000 " + "zzzzzz");
-      find_password(args);
-      break;
-    }
-
-    default: {
-      std::cout << "Invalid option!" << std::endl;
-      break;
-    }
-    }
+  switch (option) {
+  case '2': {
+    std::cout << "Input 6 bytes of password. Symbols a-z and 0-9 are permitted."
+              << std::endl;
     std::cout << ">> ";
-    std::cin >> option;
+    std::string pswd;
+    std::cin >> pswd;
+    auto res = check_line(pswd, 'p');
+    if (!res) {
+      std::cout << "Invalid password!" << std::endl;
+      break;
+    }
+
+    std::cout << "Hash for the password is: " << md5(pswd) << std::endl;
+    break;
+  }
+
+  case '3': {
+    std::cout
+        << "Input 32 symbols of hash-sum. Symbols a-f and 0-9 are permitted."
+        << std::endl;
+    std::cout << ">> ";
+    std::string hash;
+    std::cin >> hash;
+
+    auto res = check_line(hash, 'h');
+    if (!res) {
+      std::cout << "Invalid hash!" << std::endl;
+      break;
+    }
+
+    res = iter_bytes("000000", *res, "zzzzzz");
+    if (!res) {
+      std::cout << "Failed to find password!" << std::endl;
+      break;
+    }
+
+    std::cout << "Found password is: " << *res << std::endl;
+    break;
+  }
+
+  case '4': {
+    std::cout
+        << "Input 32 symbols of hash-sum. Symbols a-f and 0-9 are permitted."
+        << std::endl;
+    std::cout << ">> ";
+    std::string hash;
+    std::cin >> hash;
+
+    auto res = check_line(hash, 'h');
+    if (!res) {
+      std::cout << "Invalid hash!" << std::endl;
+      exit(1);
+    }
+
+    int fds[2];
+    if (pipe(fds) != 0) {
+      std::cout << "Error while creating pipes!" << std::endl;
+      exit(1);
+    }
+
+    int pid1 = fork();
+    if (pid1 < 0) {
+      std::cout << "Error while creating child process!" << std::endl;
+      exit(1);
+    }
+    // main process
+    else if (pid1 > 0) {
+      close(fds[1]);
+      int fds1[2];
+      if (pipe(fds1) != 0) {
+        std::cout << "Error while creating pipes!" << std::endl;
+        exit(1);
+      }
+
+      int pid = fork();
+      if (pid < 0) {
+        std::cout << "Error while splitting the child!" << std::endl;
+        exit(1);
+      }
+
+      else if (pid > 0) { // main
+        res = iter_bytes("000000", hash, "8zzzzz");
+        if (res) {
+          std::cout << "Found password: " << *res << std::endl;
+          return 0;
+        }
+
+        char pswd[6];
+        read(fds[0], pswd, 6);
+        close(fds[0]);
+        auto r = std::string{pswd};
+        if (!r.empty()) {
+          std::cout << "Found password is: " << r << std::endl;
+        } else {
+          read(fds1[0], pswd, 6);
+          close(fds1[0]);
+          r = std::string{pswd};
+          if (!r.empty()) {
+            std::cout << "Found password is: " << r << std::endl;
+          }
+        }
+
+        break;
+      } else { // another child of main
+        close(fds[0]);
+        close(fds[1]);
+        close(fds1[0]);
+        res = iter_bytes("i00000", hash, "qzzzzz");
+        if (res) {
+          write(fds1[1], (*res).c_str(), 6);
+        }
+
+        close(fds1[1]);
+      }
+
+    } else { // child process
+      int fds1[2];
+      std::cout << "Run child process" << std::endl;
+      int pid = fork();
+      if (pid < 0) {
+        std::cout << "Error while splitting the child!" << std::endl;
+        exit(1);
+      }
+
+      else if (pid > 0) { // same child process
+        close(fds[0]);
+        res = iter_bytes("900000", hash, "hzzzzz");
+        if (res) {
+          std::cout << "Password found: " << *res << std::endl;
+          write(fds[1], (*res).c_str(), 6);
+          exit(0);
+        }
+
+        char pswd[6];
+        read(fds1[0], pswd, 6); // check if fds1 contains our password
+        auto r = std::string{pswd};
+        if (!r.empty()) {
+          write(fds[1], pswd, 6);
+        }
+
+        close(fds1[1]);
+      }
+
+      else { // child of the child
+        close(fds[0]);
+        close(fds[1]);
+        res = iter_bytes("r00000", hash, "zzzzzz");
+        if (res) {
+          write(fds1[1], (*res).c_str(), 6);
+        }
+
+        close(fds1[1]);
+      }
+    }
+    break;
+  }
+
+  default: {
+    std::cout << "Invalid option!" << std::endl;
+    break;
+  }
   }
 
   return 0;
