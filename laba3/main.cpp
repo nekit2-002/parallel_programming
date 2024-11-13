80f0a648d6675d98aed09084e6ff818dcdf87872
Nikita Kosenko <foxidalim@gmail.com>
2024-11-13 14:07:35 +0300

    Make all processes terminate eventually.

--- laba3/main.cpp
+++ laba3/main.cpp
@@ -71,6 +71,58 @@ std::optional<std::string> iter_bytes(std::string pswd, std::string hash,
   return std::nullopt;
 }
 
+std::optional<std::string> iter_bytes(std::string pswd, std::string hash,
+                                      std::string stop_word, int pids[3]) {
+  int i = 5;
+  while (pswd != stop_word) {
+    for (int i = 0; i < 3; i++) {
+      if (kill(pids[i], 0) != 0) {
+        for (int j = 0; j < 3; j++) {
+          if (j != i) {
+            kill(pids[j], SIGTERM);
+          }
+        }
+        MPI_Finalize();
+        exit(0);
+      }
+    }
+    if (hash == md5(pswd)) {
+      return std::optional<std::string>{pswd};
+    }
+
+    if (pswd.substr(i, 6 - i) != stop_word.substr(i, 6 - i)) {
+      pswd[i] += 1;
+      if (pswd[i] == ':') {
+        pswd[i] += 39;
+      }
+
+      i = 5;
+      continue;
+    }
+
+    int c = 0;
+    for (int idx = 5; idx >= 0; idx--) {
+      if (pswd[idx] != 'z') {
+        break;
+      }
+
+      c += 1;
+    }
+
+    i -= c;
+
+    for (int i2 = i + 1; i2 < 6; i2++) {
+      pswd[i2] = '0';
+    }
+  }
+
+  if (hash == md5(pswd)) {
+    return std::optional<std::string>{pswd};
+  }
+
+  return std::nullopt;
+}
+
 int main(int argc, char *argv[]) {
   MPI_Init(&argc, &argv);
   std::string starts[4] = {"000000", "900000", "i00000", "r00000"};
@@ -79,16 +131,35 @@ int main(int argc, char *argv[]) {
   auto hash = md5(pswd);
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
   for (int i = 0; i < 4; i++) {
-    if (rank == i) {
-      auto res = iter_bytes(starts[i], hash, ends[i]);
+    if (i == 0 && i == rank) {
+      int pids[3];
+      for (int i = 1; i < 4; i++) {
+        MPI_Recv(&pids[i - 1], 1, MPI_INT, i, 0, MPI_COMM_WORLD,
+                 MPI_STATUS_IGNORE);
+      }
+
+      auto res = iter_bytes(starts[0], hash, ends[0], pids);
       if (res) {
         std::cout << "Password found: " << *res << std::endl;
-        MPI_Finalize();
-        exit(0);
+      }
+
+      for (int i = 0; i < 3; i++) {
+        std::cout << "Recieved pid is:" << pids[i] << std::endl;
+        kill(pids[i], SIGTERM);
+      }
+    } else {
+      if (rank == i) {
+        int current = getpid();
+        MPI_Send(&current, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        std::cout << "For rank " << rank << " pid is: " << current << std::endl;
+        auto res = iter_bytes(starts[i], hash, ends[i]);
+        if (res) {
+          std::cout << "Password found: " << *res << std::endl;
+        }
       }
     }
   }
-
   return 0;
 }
